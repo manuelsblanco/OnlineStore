b55cebe0e07c21bc39ea1f8eb0b8c9b15f20e9fd
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-04-15 15:55:10 -0300

    First Version of order.model.ts

---
+++ src/Model/order.model.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { empty } from "rxjs";
+import { Cart } from "src/app/store/cart.model";
+
+@Injectable()
+export class Order
+{
+  public id: number=-1;
+  public name: string="";
+  public address: string="";
+  public city: string="";
+  public state: string="";
+  public zip: string="";
+  public country: string="";
+  public shipped: boolean=false;
+
+  constructor(public cart: Cart)
+  {
+
+  }
+
+  clear()
+  {
+    this.id = 0;
+    this.name= "";
+    this.address= "";
+    this.city = "";
+    this.state="";
+    this.zip="";
+    this.country="";
+    this.shipped=false;
+    this.cart.clear();
+  }
+}
+
