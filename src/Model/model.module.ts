610f75f4dbc189a534bca625b3214d931f9301e7
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-04-15 16:32:29 -0300

    method submitOrder version01

--- src/Model/model.module.ts
+++ src/Model/model.module.ts
@@ -2,12 +2,18 @@ import {ProductRepository} from './product.repository';
 import {StaticDataSource} from './static.datasource';
 import {NgModule, ɵɵinjectPipeChangeDetectorRef} from '@angular/core';
 import { Cart } from 'src/app/store/cart.model';
+import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
 
 //import { CartLine } from 'src/app/store/cart.model';
 
 @NgModule
 ({
-  providers:[ProductRepository,StaticDataSource, Cart]
+  providers:[ProductRepository,
+             StaticDataSource,
+             Cart,
+             Order,
+             OrderRepository]
 })
 
 export class ModelModule
--- src/app/chech-out/chech-out.component.ts
+++ src/app/chech-out/chech-out.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/Model/order.model';
+import { OrderRepository } from 'src/Model/order.repository';
+
 
 @Component({
   templateUrl: './chech-out.component.html'
 })
 export class ChechOutComponent implements OnInit {
 
-  constructor() { }
+  orderSent:boolean=false;
+  submitted:boolean=false;
+
+
+  constructor(public repository: OrderRepository,
+              public order: Order)
+  {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  submitOrder(form: NgForm)
+  {
+    this.submitted=true;
+    if(form.valid)
+    {
+      this.repository.saveOrder(this.order)
+      .subscribe(order =>{
+        this.order.clear();
+        this.orderSent=true;
+        this.submitted=false;
+      })
+    }
   }
 
 }
